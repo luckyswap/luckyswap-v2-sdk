0b0e57127bc5b30ddd66690458e0d93b8748322f
lucky-kh1em <khiem.na@luckytech.io>
2021-07-03 16:33:17 +0700

    create dictionary router & factory

--- dist/entities/addresses.d.ts
+++
@@ -1,7 +0,0 @@
-import { ChainId } from './../constants';
-export declare const FACTORY_ADDRESSES: {
-    [chainId in ChainId]: string;
-};
-export declare const ROUTER_ADDRESSES: {
-    [chainId in ChainId]: string;
-};
